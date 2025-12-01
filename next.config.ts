01b195b60789fcad423327ceda084e2e9ea12441
Aldo Gomez <gomezfx@gmail.com>
2025-11-30 23:14:46 -0600

    Fix thumbnail image local pattern

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     localPatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
         pathname: "/api/thumbnail",
         // Allow any thumbnail request identified by an id query param
         search: "id=**",
